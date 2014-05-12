5dc35386a92453a5b2a51abc55112b557a80fe22
Robert Bollons <rob@robbollons.com>
2014-05-12 13:36:35 +0100

    Fixed null refrence errors for TeamCity 8.1.2 and TFS provider - referencing #1

--- Script/ViewModels/BaseBuild.js
+++ Script/ViewModels/BaseBuild.js
@@ -13,7 +13,32 @@
         }, self);
 
         self.isRunning = ko.computed(function () {
-            return this.running && this.running();
+            return this.running && this.running() && this.percentageComplete;
+        }, self);
+
+        self.hasProgress = ko.computed(function () {
+            if (this.percentageComplete)
+                return true;
+            else
+                return false;
+        }, self);
+
+        self.hasBranchName = ko.computed(function() {
+            if (this.branchName)
+                return true;
+            else
+                return false;
+        }, self);
+
+        self.description = ko.computed(function() {
+            var description = '';
+            if (this.branchName)
+                description += this.branchName() + ' branch - ';
+
+            if (this.number)
+                description += '#' + this.number()
+
+            return description;
         }, self);
     }
 });
