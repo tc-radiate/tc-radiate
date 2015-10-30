0ff7c5cdca64436efca373ec1a2d789ed9c24ac3
Slawomir.Brzezinski <slawomir.brzezinski@travcorpuk.com>
2015-10-30 11:19:37 +0000

    Make it possible to develop the local files and execute them against actual remote TeamCity instance (by disabling cross domain security)  - added shortcuts to do it in Chrome and Firefox. Each have their own quirks to using the no-security mode, so added messages to the batch files.

---
+++ open-in-chrome-without-web-security-for-local-development.cmd
@@ -0,0 +1,3 @@
+@echo Make sure that all your Chrome windows are closed. After it launches, DON'T USE THIS OR ANY OTHER CHROME WINDOW FOR ANYTHING ELSE THAN VIEWING THE FILE! Once you finished developing, CLOSE ALL YOUR CHROME WINDOWS AGAIN (should be just one, if you followed :) and ONLY THEN launch your normal Chrome Shortcut. Unless you want to turn you into a hobo in a split second! Any website you visit can take all the money from your bank account and send your emails to yor boss!
+@pause
+start "" "%ProgramFiles(x86)%\Google/Chrome/Application/chrome.exe" --disable-web-security %~dp0index.html
