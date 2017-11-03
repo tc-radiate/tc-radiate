da2a5cccbdd6629ae65f935d8cbcec026bd7e8e0
slawomir-brzezinski-at-interxion <slawomir-brzezinski-at-interxion@users.noreply.github.com>
2017-11-03 10:13:19 +0000

    Point to Interxion's TeamCity

--- Settings.js
+++ Settings.js
@@ -1,6 +1,6 @@
 ﻿var organizationSettings = {
     //The url that points to team city
-    teamCityUrl: 'https://todo.edit.this.url.invalid',
+    teamCityUrl: 'http://teamcity.office.interxion.net',
 
     //Only show builds for branches that satisfy the predicate
     branchFilter: function(branch) {
